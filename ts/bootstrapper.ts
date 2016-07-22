da813cac9c40555fd82611e43e23139ed6980413
avj2352 <avj2352@gmail.com>
2016-07-22 08:49:33 +0530

    Resolved issue: coin is not defined

--- ts/bootstrapper.ts
+++ ts/bootstrapper.ts
@@ -3,6 +3,9 @@
 //This will instantiate all classes
 //Setup the data-binding
 
-/// <reference path="vendingMachine.ts"/>
+/// <reference path="./vendingMachine.ts"/>
+/// <reference path="./coin.ts"/>
 var machine = new VendingMachine();
+var coin = new Quarter();
+ko.applyBindings(coin);
 ko.applyBindings(machine); //Typescript doesn't understand what ko object is...
